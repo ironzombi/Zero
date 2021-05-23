c4d984a117e33fc1e3f572ba7960d4d32d22f895
ironzombi <span@ironzombi.com>
2021-05-23 14:52:00 +0800

    Making a mess
    I have the utter basics but I am not storing anything, need to re-read chapter 7 that part about pointers, or maybe I just have to implement some form of DB

--- cmd/auth/login.go
+++ cmd/auth/login.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 	"net/http"
 	"text/template"
+	"time"
 )
+
+type PiHosts struct {
+	addr     []string
+	hostname []string
+}
+
+var Hosts PiHosts
 
 /* while this is a login function it is more just
  * to work with input via html page served with golang */
 func Login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method) //get request method
+	cTime := time.Now()
+	fmt.Printf("%s method:%s\n", cTime.Format("2006.01.02 15:04:05"), r.Method) //get request method
 	if r.Method == "GET" {
 		t, _ := template.ParseFiles("site/index.html")
 		t.Execute(w, nil)
@@ -23,6 +32,29 @@ func Login(w http.ResponseWriter, r *http.Request) {
 			f.Execute(w, nil)
 		}
 	}
+}
+
+// TODO: this currently doesnt work, dl.Datalistener is empty when called.
+func Render(w http.ResponseWriter, req *http.Request) {
+
+	if req.Method == "GET" {
+		t, _ := template.ParseFiles("site/info.html")
+		t.Execute(w, nil)
+	} else {
+		req.ParseForm()
+		Hosts.addr = req.Form["ipaddress"]
+		Hosts.hostname = req.Form["hostname"]
+		t, err := template.New("MIPHACK").Parse("Zero \"{{ .hostname}}\"")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		t.Execute(w, Hosts)
+	}
+}
+
+func ListServers(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Raspberry Pi Zeros are awesome: %s", Hosts.hostname)
 }
 
 // TODO: creat an actual password check maybe.
--- cmd/serve.go
+++ cmd/serve.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"text/template"
 	login "zero/cmd/auth"
 )
 
@@ -27,24 +26,14 @@ func (dl DataListener) Default(w http.ResponseWriter, req *http.Request) {
 	http.ServeFile(w, req, "index.html")
 }
 
-// TODO: this currently doesnt work, dl.Datalistener is empty when called.
-func (dl *DataListener) Template(w http.ResponseWriter, req *http.Request) {
-
-	t, err := template.New("MIPHACK").Parse("Input \"{{ .dl.input}}\"")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	t.Execute(w, dl)
-}
-
 func ZeroListener() {
 	var dl DataListener
 	http.HandleFunc("/", dl.Default)
 	http.HandleFunc("/add", dl.Input)
 	http.HandleFunc("/echo", dl.Echo)
-	http.HandleFunc("/template", dl.Template)
+	http.HandleFunc("/template", login.Render)
 	http.HandleFunc("/login", login.Login)
+	http.HandleFunc("/info", login.ListServers)
 
 	log.Fatal(http.ListenAndServe("0.0.0.0:8181", nil))
 
