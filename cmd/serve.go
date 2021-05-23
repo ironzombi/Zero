77db90a67cf60d3604fe9908d7e9345416694787
ironzombi <span@ironzombi.com>
2021-05-23 11:05:49 +0800

    Refactor the meat
    move the handlers and methods to cmd/serve

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,7 +1,49 @@
 package serve
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+type DataListener struct {
+	input  string
+	output string
+	size   int
+}
+
+func (dl DataListener) Input(w http.ResponseWriter, req *http.Request) {
+	dl.input = req.URL.Query().Get("input")
+	fmt.Fprintf(w, "%s", dl.input)
+}
+
+func (dl DataListener) Echo(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "this worked,  %s", req.URL)
+}
+
+func (dl DataListener) Default(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "index.html")
+}
+
+// TODO: this currently doesnt work, dl.Datalistener is empty when called.
+func (dl *DataListener) Template(w http.ResponseWriter, req *http.Request) {
+
+	t, err := template.New("MIPHACK").Parse("Input \"{{ .dl.input}}\"")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	t.Execute(w, dl)
+}
 
 func ZeroListener() {
+	var dl DataListener
+	http.HandleFunc("/", dl.Default)
+	http.HandleFunc("/add", dl.Input)
+	http.HandleFunc("/echo", dl.Echo)
+	http.HandleFunc("/template", dl.Template)
+
+	log.Fatal(http.ListenAndServe("192.168.1.117:8181", nil))
 	fmt.Println("check good")
 }
--- main.go
+++ main.go
@@ -1,48 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-
 	serve "zero/cmd"
 )
 
-type DataListener struct {
-	input  string
-	output string
-	size   int
-}
-
-func (dl DataListener) Input(w http.ResponseWriter, req *http.Request) {
-	dl.input = req.URL.Query().Get("input")
-	fmt.Fprintf(w, "%s", dl.input)
-}
-
-func (dl DataListener) Echo(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "this worked,  %s", req.URL)
-}
-
-func (dl DataListener) Default(w http.ResponseWriter, req *http.Request) {
-	http.ServeFile(w, req, "index.html")
-}
-
-// using a template, wont return anything  DataListener will be nil/nil
-func (dl DataListener) Template(w http.ResponseWriter, req *http.Request) {
-	tmplt := template.New("Hello World")
-	tmplt, _ = tmplt.Parse("Top Secret: {{.Id}} - {{.Name}}!")
-	p := DataListener{size: 1, output: "secret"}
-	tmplt.Execute(w, p)
-}
-
 func main() {
-	var dl DataListener
 
-	http.HandleFunc("/", dl.Default)
-	http.HandleFunc("/add", dl.Input)
-	http.HandleFunc("/echo", dl.Echo)
-	http.HandleFunc("/template", dl.Template)
-	log.Fatal(http.ListenAndServe("192.168.1.117:8181", nil))
 	serve.ZeroListener()
 }
